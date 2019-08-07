621eeb47ee47e57624bdcce3d29344e3ced45fa4
Jeremy Busk <jeremybusk@gmail.com>
2019-08-07 11:18:02 -0600

    Create windows10usb.sh

---
+++ windows10usb.sh
@@ -0,0 +1,11 @@
+# Create bootable usb windows 10
+
+https://github.com/slacka/WoeUSB
+```
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt update
+sudo apt install woeusb
+# Remove if you don't need other software
+sudo add-apt-repository --remove ppa:nilarimogard/webupd8
+sudo apt update
+```
